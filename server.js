ab2861e7214c279b53fb4512332686b9956fa680
Pablo Contreras <pacg1991@gmail.com>
2021-07-13 01:24:50 -0500

    first create read mongodb

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
+const MongoClient = require('mongodb').MongoClient
+const connectionString = process.env.NODE_DB;
 
 // Make sure you place body-parser before your CRUD handlers!
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,9 +17,69 @@ app.listen(3000, function() {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-app.post('/quotes', (req, res) => {
+
+app.get('/map-events', async (req, res) => {
+    console.log('req',req)
+
+    try {
+        
+        const client = await MongoClient.connect(connectionString);
+        // const MyCollection = db.collection('MyCollection');
+        // const result = await MyCollection.find().toArray();
+
+        console.log('client',client)
+
+        const db = client.db('map-events')
+        console.log('db',db)
+
+        const events = await db.collection('events').find().toArray()
+        console.log('events',events)
+        
+        res.send({ list: events })
+
+    } catch (err) {
+        console.log('err',err)
+        res.send({ errors: [''] })
+    }
+
+})
+app.post('/map-events', async (req, res) => {
     console.log('req',req)
 
     const { body } = req
-    res.send({ body })
+
+    try {
+        
+        const client = await MongoClient.connect(connectionString);
+        // const MyCollection = db.collection('MyCollection');
+        // const result = await MyCollection.find().toArray();
+
+        console.log('client',client)
+
+        const db = client.db('map-events')
+        console.log('db',db)
+        // console.log('result',result)
+
+        const eventsCollection = db.collection('events')
+
+        eventsCollection.insertOne(body)
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => console.error(error))
+
+        res.send({ body })
+
+    } catch (err) {
+        console.log('err',err)
+        res.send({ errors: [''] })
+    }
+
 })
+
+// MongoClient.connect(connectionString, {
+//     useUnifiedTopology: true
+// }, (err, client) => {
+//     if (err) return console.error(err)
+//     console.log('Connected to Database')
+// })
