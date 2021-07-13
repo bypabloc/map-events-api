1a01f8c9ee269779cd28ba8c439ec91140c05a02
Pablo Contreras <pacg1991@gmail.com>
2021-07-13 00:19:45 -0500

    first get post

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
+// All your handlers here...
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+app.post('/quotes', (req, res) => {
+    console.log('req',req)
+
+    const { body } = req
+    res.send({ body })
 })
